68580651f50fd5eb9e945ef79e0eb66edcfb5eb2
Travis297 <travis.witts@outlook.com>
2020-12-14 18:30:13 +1030

    adding prompts

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passLength = prompt("How long would you like your password to be? (8-128)")
+  var upper = confirm("Would you like to include uppercase numbers?")
+  var special = confirm("Would you like to include special characters?")
+  var numeric = confirm("Would you like to include numbers?")
+  var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var genArray = []
+  if (upper) {
+    genArray = genArray.concat(lower.map(letter => letter.toUpperCase()))
+  }
+  if (special) {
+
+  }
+}
