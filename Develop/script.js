1e96107120f6ab34e306055a1f2ec3e58a92dfc2
Travis297 <travis.witts@outlook.com>
2020-12-15 22:55:21 +1030

    Finishing generator

--- Develop/script.js
+++ Develop/script.js
@@ -15,16 +15,40 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var passLength = prompt("How long would you like your password to be? (8-128)");
+  while (passLength > 128 || passLength < 8) {
+    alert("Please pick a value between 1-128");
+    passLength = prompt("How long would you like your password to be? (8-128)");
+  }
   var upper = confirm("Would you like to include uppercase numbers?");
   var special = confirm("Would you like to include special characters?");
   var numeric = confirm("Would you like to include numbers?");
-  var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var special = [" ", "!" '"', "'", "#", ]
-  var genArray = [];
+  var genArray = arrayBuilder(97, 122);
+  var passwordArr = [];
   if (upper) {
-    genArray = genArray.concat(lower.map(letter => letter.toUpperCase()));
+    genArray = genArray.concat(arrayBuilder(65, 90));
+  }
+  if (numeric) {
+    genArray = genArray.concat(arrayBuilder(48, 57));
   }
   if (special) {
+    var symbols = arrayBuilder(33, 47).concat(58, 64).concat(91,96).concat(123, 126);
+    genArray = genArray.concat(symbols);
+  }
+  for (i = 0; i < passLength; i++) {
+    var character = genArray[Math.floor(Math.random() * genArray.length)];
+    passwordArr.push(String.fromCharCode(character));
+  }
+  var newPassword = passwordArr.join('');
+  return newPassword;
+}
+
+
+
+function arrayBuilder(lowVal, highVal) {
+  var array = [];
+  for (var i = lowVal; i <= highVal; i++) {
+    array.push(i);
 
   }
+  return array
 }
