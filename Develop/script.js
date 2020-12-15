3701a9f6abfb8cf3ba50f467f3e41359a9dffedc
Travis297 <travis.witts@outlook.com>
2020-12-15 23:02:34 +1030

    adding comments

--- Develop/script.js
+++ Develop/script.js
@@ -15,15 +15,20 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var passLength = prompt("How long would you like your password to be? (8-128)");
+  // Asking for a password length then it will continue to ask until it is a character between 8-128
   while (passLength > 128 || passLength < 8) {
-    alert("Please pick a value between 1-128");
+    alert("Invalid choice! Please pick a value between 1-128");
     passLength = prompt("How long would you like your password to be? (8-128)");
   }
+  // Confirm values to decide what types of characters will be used in the password.
   var upper = confirm("Would you like to include uppercase numbers?");
   var special = confirm("Would you like to include special characters?");
   var numeric = confirm("Would you like to include numbers?");
+  // An initial array is created containing lowercase numbers.
   var genArray = arrayBuilder(97, 122);
+  // Empty array to house the generated password after the for loop
   var passwordArr = [];
+  // Multiple if statements to concatenate extra characters which were chosen in the above values.
   if (upper) {
     genArray = genArray.concat(arrayBuilder(65, 90));
   }
@@ -34,16 +39,17 @@ function generatePassword() {
     var symbols = arrayBuilder(33, 47).concat(58, 64).concat(91,96).concat(123, 126);
     genArray = genArray.concat(symbols);
   }
+  // The main for loop of the function, randomly selects a number in the array using the ASCII numbering scheme and the inbuilt Math function.
   for (i = 0; i < passLength; i++) {
     var character = genArray[Math.floor(Math.random() * genArray.length)];
+    // Uses the string functions to translate to ASCII, can also be done by concatenating "&#" + char + ";"
     passwordArr.push(String.fromCharCode(character));
   }
+  // Building the new string to be displayed as the password.
   var newPassword = passwordArr.join('');
   return newPassword;
 }
-
-
-
+// This function takes a highest and lowest value and creates an array of all the numbers inclusively.
 function arrayBuilder(lowVal, highVal) {
   var array = [];
   for (var i = lowVal; i <= highVal; i++) {
